5b6037e5f571e7849c8d5dba66a7fdb07f286150
Bernardash <bernard.programmer@gmail.com>
2022-11-17 03:22:27 -0300

    api teste

--- backend/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
@@ -1,12 +1,18 @@
-package com.devsuperior.config;
+package com.devsuperior.dsmeta.config;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -35,4 +41,5 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 }
